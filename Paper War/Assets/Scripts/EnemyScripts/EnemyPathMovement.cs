be75594413f5d21e1f7826614337433a4c252a01
Bruno Grandis <brunograndis@gmail.com>
2021-06-11 08:41:07 +0100

    DEUS NÃO EXISTE
    Tentei implementar um inimigo novo, não sei pq n tá funcionando. Que ódio.

---
+++ Paper War/Assets/Scripts/EnemyScripts/EnemyPathMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathMovement : MonoBehaviour
+{
+
+    public Transform[] points;
+    public float speed;
+    int current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       if (transform.position != points[current].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed * Time.deltaTime);
+        }
+       else
+        {
+            current = (current + 1) % points.Length;
+        }
+    }
+}
+
